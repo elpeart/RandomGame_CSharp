a92cb388da8ee75868d49b4fc6b6f95ae6ffcb79
Eric Peart <elpeart@gmail.com>
2021-10-22 22:15:05 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace RandomGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int randomNumber = 62;
+			int guess;
+			int numGuesses = 0;
+
+			do
+			{
+				Console.WriteLine("Enter your guess");
+				guess = Convert.ToInt32(Console.ReadLine());
+				numGuesses++;
+
+				if (guess == randomNumber)
+				{
+					Console.WriteLine("You Win!");
+				}
+				else if (guess > randomNumber)
+				{
+					Console.WriteLine("Too high");
+				}
+				else if (guess < randomNumber)
+				{
+					Console.WriteLine("Too low");
+				}
+
+				if (numGuesses > 10)
+				{
+					Console.WriteLine("You Lose!");
+					break;
+				}
+			}
+			while (guess != randomNumber);
+		}
+	}
+}
